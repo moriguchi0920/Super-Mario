1b6f323d0333738c32f9db41b3747564747d259f
504-40\cre <a>
2026-06-06 19:26:58 +0900

    ブロックの固定化の実装(スクロール時に一緒についてこない)

--- SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.cpp
+++ SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.cpp
@@ -5,13 +5,21 @@
 
 hatenablock::hatenablock(float x, float y) : Object(ObjectManager::makeId())
 {
+
+    addComponent<ComponentTransform>(id).lock()->setScroll(true);
+
+    auto comT = getComponent<ComponentTransform>();
+    comT.lock()->setPosition(Point(x, y));
+
     // 四角描画コンポーネント追加
     auto rect = addComponent<ComponentRenderableRect>(getId(), 0.5f, Float2(x, y), Float2(SPRITE_SIZE, SPRITE_SIZE));
 
-    // 黄色設定
-    rect.lock()->setColor(250, 220, 0);
+    auto comR = getComponent<ComponentRenderableRect>();
+    comR.lock()->setColor(250, 220, 0);
 }
 
 void hatenablock::update()
 {
+    auto comR = getComponent<ComponentRenderableRect>();
+    comR.lock()->syncFromTransform();
 }
--- SuperMarioProject/DxLibTemplate_ver0.1/rengablock.cpp
+++ SuperMarioProject/DxLibTemplate_ver0.1/rengablock.cpp
@@ -1,17 +1,23 @@
 #include "rengablock.h"
 #include "objectManager.h"
-#include"componentTransform.h"
+#include "componentTransform.h"
 #include "componentRenderable.h"
 
-rengablock::rengablock(float x, float y): Object(ObjectManager::makeId())
+rengablock::rengablock(float x, float y) : Object(ObjectManager::makeId())
 {
-    // 四角描画コンポーネント追加
-    auto rect = addComponent<ComponentRenderableRect>(getId(),0.5f,Float2(x, y),Float2(SPRITE_SIZE, SPRITE_SIZE));
+    addComponent<ComponentTransform>(id).lock()->setScroll(true);
 
-    // 茶色設定
-    rect.lock()->setColor(139, 69, 19);
+    auto comT = getComponent<ComponentTransform>();
+    comT.lock()->setPosition(Point(x, y));
+
+    addComponent<ComponentRenderableRect>(this->id,0.5f,Float2(x, y),Float2(SPRITE_SIZE, SPRITE_SIZE));
+
+    auto comR = getComponent<ComponentRenderableRect>();
+    comR.lock()->setColor(139, 69, 19);
 }
 
 void rengablock::update()
 {
+    auto comR = getComponent<ComponentRenderableRect>();
+    comR.lock()->syncFromTransform();
 }
