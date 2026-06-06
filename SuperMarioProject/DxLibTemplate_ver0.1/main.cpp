51d47f93176d3c800e9de460daea874e0e1c9c2b
504-40\cre <a>
2026-06-06 19:30:59 +0900

    blockの軽微な配置変更

--- SuperMarioProject/DxLibTemplate_ver0.1/main.cpp
+++ SuperMarioProject/DxLibTemplate_ver0.1/main.cpp
@@ -45,8 +45,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ObjectManager::createObject<Dokan>(100.0f, 160.0f);
 
 	ObjectManager::createObject<Mario>();
-	ObjectManager::createObject<rengablock>(50.0f, 130.0f);
-	ObjectManager::createObject<hatenablock>(80.0f, 130.0f);
+	ObjectManager::createObject<rengablock>(80.0f, 130.0f);
+	ObjectManager::createObject<hatenablock>(50.0f, 130.0f);
 
 	
 
