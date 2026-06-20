d2cb5ec2aea4743a6a8d62e5950f70511f02206e
504-40\cre <a>
2026-06-20 16:28:40 +0900

    バグ修正

--- SuperMarioProject/DxLibTemplate_ver0.1/floor.h
+++ SuperMarioProject/DxLibTemplate_ver0.1/floor.h
@@ -4,21 +4,11 @@
 
 class Floor : public BlockBase
 {
-	enum Type {
-		Ground1,
-		Ground2,
-		Ground3,
-		Ground4
-	};
 
 public:
 	Floor(float x, float width);
 	virtual ~Floor();
 
 	void update() override;
-
-private:
-	static Type g_nextType;
-	static Point g_nextPos;
 
 };
