52150d5471345e935a9bd17d57364f6b76df24ad
505-05\cre <oca2502sg0001@edu.oca.ac.jp>
2026-06-20 14:34:19 +0900

    ハテナブロックをたたいた時に、基本をコイン、指定したブロックからはキノコが出るようにしました また、一度たたいたハテナブロックは茶色になるよう変更しました

--- SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.cpp
+++ SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.cpp
@@ -4,40 +4,44 @@
 #include "componentRenderable.h"
 #include "superMushroom.h" 
 #include "const.h"
-#include"scrollManager.h"
+#include "scrollManager.h"
 
-hatenablock::hatenablock(Rect rect) : BlockBase(rect)
+
+hatenablock::hatenablock(Rect rect, BLOCK_ITEM_TYPE type) : BlockBase(rect)
 {
+    myItemType = type;
+
     auto transform = getComponent<ComponentTransform>();
 
-        if (!transform.expired())
-        {
-            transform.lock()->setScroll(true);
-        }
-
-        auto colRect = getComponent<ComponentCollisionRect>();
-        if (!colRect.expired())
-        {
-            colRect.lock()->addTag(ICollisionTag::BLOCK);
-        }
-
-        auto renderRect = addComponent<ComponentRenderableRect>(id, 0.5, rect);
-
-        if (!renderRect.expired())
-        {
-            // â©êFê›íË
-            renderRect.lock()->setColor(250, 220, 0);
-        }
+    if (!transform.expired())
+    {
+        transform.lock()->setScroll(true);
     }
+
+    auto colRect = getComponent<ComponentCollisionRect>();
+    if (!colRect.expired())
+    {
+        colRect.lock()->addTag(ICollisionTag::BLOCK);
+    }
+
+    auto renderRect = addComponent<ComponentRenderableRect>(id, 0.5, rect);
+
+    if (!renderRect.expired())
+    {
+        
+        renderRect.lock()->setColor(250, 220, 0);
+    }
+}
 
 void hatenablock::update()
 {
-
     BlockBase::update();
 
     auto comR = getComponent<ComponentRenderableRect>();
-    comR.lock()->syncFromTransform();
-
+    if (!comR.expired()) 
+    {
+        comR.lock()->syncFromTransform();
+    }
 }
 
 void hatenablock::activateProc()
@@ -61,8 +65,14 @@ void hatenablock::hitFromBottom()
 
     m_used = true;
 
-    auto transform = getComponent<ComponentTransform>();
+  
+    auto comR = getComponent<ComponentRenderableRect>();
+    if (!comR.expired())
+    {
+        comR.lock()->setColor(150, 100, 50); 
+    }
 
+    auto transform = getComponent<ComponentTransform>();
     if (transform.expired())
     {
         return;
@@ -70,11 +80,25 @@ void hatenablock::hitFromBottom()
 
     Float2 pos = transform.lock()->getPosition();
 
-    ObjectManager::createObject<SuperMushroom>(
-        Rect(
-            Float2(pos.x, pos.y - SPRITE_SIZE),
-            Float2(SPRITE_SIZE, SPRITE_SIZE)
-        )
-    );
-}
+    
+    switch (myItemType)
+    {
+    case BLOCK_ITEM_TYPE::MUSHROOM:
+        // ÉLÉmÉRÇê∂ê¨Ç∑ÇÈ
+        ObjectManager::createObject<SuperMushroom>(
+            Rect(
+                Float2(pos.x, pos.y - SPRITE_SIZE),
+                Float2(SPRITE_SIZE, SPRITE_SIZE)
+            )
+        );
+        break;
 
+    case BLOCK_ITEM_TYPE::COIN:
+       
+        break;
+
+    case BLOCK_ITEM_TYPE::FLOWER:
+       
+        break;
+    }
+}
--- SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.h
+++ SuperMarioProject/DxLibTemplate_ver0.1/hatenablock.h
@@ -1,24 +1,32 @@
 #pragma once
 
 #include "entityObject.h"
-#include"blockBase.h"
+#include "blockBase.h"
+
+
+enum class BLOCK_ITEM_TYPE {
+    COIN,      // コイン
+    MUSHROOM,  // キノコ
+    FLOWER     // フラワー
+};
+
 
 class hatenablock : public BlockBase
 {
 public:
-    hatenablock(Rect rect);
+   
+    hatenablock(Rect rect, BLOCK_ITEM_TYPE type = BLOCK_ITEM_TYPE::COIN);
 
     void update() override;
-
     void activateProc() override;
-
     void deactivateProc() override;
-
     void deathProc() override;
 
     void hitFromBottom();
 
 private:
-    bool m_used = false;
+    bool m_used = false; // 叩かれているかどうか
 
+   
+    BLOCK_ITEM_TYPE myItemType;
 };
